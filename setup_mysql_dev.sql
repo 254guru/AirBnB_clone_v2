b5861085aea08bb92239e0a46744f2c7f7118401
254guru <otienooluda@gmail.com>
2023-11-23 06:35:40 -0800

    update

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,13 @@
 -- script that prepares a MySQL server
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
+-- Create or update hbnb_dev user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
+-- Grant privileges to hbnb_dev on hbnb_dev_db
 GRANT ALL PRIVILEDGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
-GRANT SELECT ON perfomance_schema.* TO 'hbnb_dev'@'localhost';
+-- Grant SELECT privilege on performance_schema to hbnb_dev
+GRANT SELECT ON `perfomance_schema`.* TO 'hbnb_dev'@'localhost';
 
 FLUSH PRIVILEDGES;

4fdfc97c52cc529646372a55ad76ebaff94b73d0
254guru <otienooluda@gmail.com>
2023-11-23 06:55:04 -0800

    update

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -8,7 +8,7 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
 
 -- Grant SELECT privilege on performance_schema to hbnb_dev
-GRANT SELECT ON `perfomance_schema`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 
 -- flush privileges
 FLUSH PRIVILEGES;

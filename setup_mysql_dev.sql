1e0579888802d40aeaf171853edbea936d611285
254guru <otienooluda@gmail.com>
2023-11-23 06:37:20 -0800

    update

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,7 +5,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Grant privileges to hbnb_dev on hbnb_dev_db
-GRANT ALL PRIVILEDGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEDGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
 
 -- Grant SELECT privilege on performance_schema to hbnb_dev
 GRANT SELECT ON `perfomance_schema`.* TO 'hbnb_dev'@'localhost';

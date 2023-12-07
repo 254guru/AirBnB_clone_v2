cc6230017a26c52dabfe9bc8ebaaf0c3ddc2c702
254guru <otienooluda@gmail.com>
2023-12-07 12:55:03 -0800

    update file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,7 +3,7 @@
 
 if ! command -v nginx &> /dev/null; then
     sudo apt update
-    sudo apt install nginx -y
+    sudo apt install -y nginx
 fi
     sudo mkdir -p /data/web_static/releases/test/
     sudo mkdir -p /data/web_static/shared/
@@ -17,6 +17,7 @@ echo "<html>
     <h1>This is a test page for Nginx configuration</h1>
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html >/dev/null
+
 # create symbolic link
 ln -fs /data/web_static/releases/test/ /data/web_static/current
 # set ownership
@@ -24,12 +25,10 @@ sudo chown -R ubuntu:ubuntu /data/
 
 # Update Nginx configuration to serve the
 # content of /data/web_static/current/
-config_file="/etc/nginx/sites-available/default"
-sudo sed -i '/^server {/a \
-\
-    location /hbnb_static {\
-        alias /data/web_static/current/;\
-    }\
-' $config_file
+
+location_header="location \/hbnb\_static\/ {"
+location_content="alias \/data\/web\_static\/current\/;"
+new_location="\n\t$location_header\n\t\t$location_content\n\t}\n"
+sed -i "37s/$/$new_location/" /etc/nginx/sites-available/default
 
 sudo service nginx restart

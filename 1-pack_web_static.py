a612b76efac89d5d4f8488724a6c0394202e82a0
254guru <otienooluda@gmail.com>
2023-12-08 14:41:39 -0800

    fabric script that distributes an archive to your web servers

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,8 +2,10 @@
 # fabric module
 from datetime import datetime
 from fabric.api import local
+from fabric import task
 
 
+@task
 def do_pack():
     """script that generates a .tgz archive from the contents
     of the web_static folder of this repository.

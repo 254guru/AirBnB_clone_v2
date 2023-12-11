57c36daa039280fba9584081cea74a5e72dfdf00
254guru <otienooluda@gmail.com>
2023-12-11 03:18:15 -0800

    full deployment

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+# Fabric script that (based on 2-do_deploy_web_static.py)
+# creates and distributes an archive
+# to your web servers using the function deploy
+
+from datetime import datetime
+from fabric.api import *
+from os import path
+
+
+env.hosts = ['35.153.231.146', '54.210.59.168']
+
+
+def do_pack():
+    d = datetime.now()
+    now = d.strftime('%Y%m%d%H%M%S')
+
+    local("mkdir -p versions")
+    local("tar -czvf versions/web_static_{}.tgz web_static".format(now))
+
+
+def do_deploy(archive_path):
+    if path.exists(archive_path):
+        archive = archive_path.split('/')[1]
+        a_path = "/tmp/{}".format(archive)
+        folder = archive.split('.')[0]
+        f_path = "/data/web_static/releases/{}/".format(folder)
+
+        put(archive_path, a_path)
+        run("mkdir -p {}".format(f_path))
+        run("tar -xzf {} -C {}".format(a_path, f_path))
+        run("rm {}".format(a_path))
+        run("mv -f {}web_static/* {}".format(f_path, f_path))
+        run("rm -rf {}web_static".format(f_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(f_path))
+
+        return True
+
+    return False
+
+
+def deploy():
+    archive_path = do_pack()
+
+    if not archive_path:
+        return False
+
+    return do_deploy(archive_path)

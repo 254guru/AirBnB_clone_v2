b184ab6a7bee5dfa63c891a28662065de55e7db3
254guru <otienooluda@gmail.com>
2023-12-31 14:17:20 -0800

    update place and amenity

--- models/place.py
+++ models/place.py
@@ -11,9 +11,9 @@ from models.amenity import Amenity, place_amenity
 
 class Place(BaseModel):
     """ A place to stay """
-       __tablename__ = "places"
+    __tablename__ = "places"
 
-    if getenv('HBNB_TYPE_STORAGE') == "db":
+    if os.getenv('HBNB_TYPE_STORAGE') == "db":
         city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
         user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
         name = Column(String(128), nullable=False)

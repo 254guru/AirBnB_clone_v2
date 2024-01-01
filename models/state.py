2fb19e46f363c7c1071db16c41b615012c299d79
254guru <otienooluda@gmail.com>
2024-01-01 06:05:36 -0800

    update codebase

--- models/state.py
+++ models/state.py
@@ -11,21 +11,15 @@ import models
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete-orphan")
 
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state", cascade="all, delete")
-
-    else:
-        name = ""
-
-        @property
-        def cities(self):
-            """
-            return the list of city objects linked to current state
-            """
-            city_list = []
-            for city in models.storage.all(City).values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+    @property
+    def cities(self):
+        """Getter attribute that returns the list of City instances."""
+        
+        city_list = []
+        for city in models.storage.all(City).values():
+            if self.id == city.state_id:
+                city_list.append(city)
+        return city_list

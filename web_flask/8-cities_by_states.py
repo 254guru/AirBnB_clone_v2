633a2346bbdd3b89a6466aff2f2676bf870352fc
254guru <otienooluda@gmail.com>
2024-01-01 07:32:41 -0800

    cities by states

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application
+"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda state: state.name)
+    cities_and_states = []
+    for state in states:
+        cities = []
+        if type(storage).__name__ == 'DBStorage':
+            cities = state.cities
+        else:
+            cities = state.cities()
+        cities = sorted(cities, key=lambda city: city.name)
+        cities_and_states.append((state, cities))
+    return render_template('cities_by_states.html',
+                           cities_and_states=cities_and_states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Closes the SQLAlchemy session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
